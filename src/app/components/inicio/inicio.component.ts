fd85301c7cd18b1140b08e97b87baaf5557e5e8f
Manuel <manuel.machado.j.m@gmail.com>
2024-10-24 20:40:57 -0300

    act241024

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SobreMiComponent } from '../sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from '../proyectos/proyectos.component';
+import { ContactoComponent } from '../contacto/contacto.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterModule, SobreMiComponent],
+  imports: [RouterModule, SobreMiComponent, ContactoComponent, ProyectosComponent],
+
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
