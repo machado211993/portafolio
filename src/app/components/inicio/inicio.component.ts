7e86d787a03a08c5ddf5f17e5029c0294312e45d
Manuel <manuel.machado.j.m@gmail.com>
2024-10-24 02:09:45 -0300

    commit3

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SobreMiComponent } from '../sobre-mi/sobre-mi.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SobreMiComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
